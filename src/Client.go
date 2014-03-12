4582d4e1659c93c19cf1799bee71dee94c9fde8a
Xavier Grand <grand.xavier@gmail.com>
2014-03-12 02:17:24 -0700

    Add global list/add/delete function

--- src/Client.go
+++ src/Client.go
@@ -15,3 +15,18 @@ func (c *Client) listIndexes() (string) {
      return c.transport.request("GET", "/1/indexes", "")
 }
 
+func (c *Client) initIndex(indexName string) *Index {
+  return NewIndex(indexName, c)
+}
+
+func (c *Client) listKeys() string {
+  return c.transport.request("GET", "/1/keys", "")
+}
+
+func (c *Client) getKey(key string) string {
+  return c.transport.request("GET", "/1/keys/" + key, "")
+}
+
+func (c *Client) deleteKey(key string) string {
+  return c.transport.request("DELETE", "/1/keys/" + key, "")
+}
