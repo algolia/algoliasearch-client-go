52fba60d46317babdca4ef2559f01b9bf6776951
Anthony Seure <anthony.seure@gmail.com>
2016-05-03 12:03:02 +0200

    Type the Index functions parameters and returned values

---
+++ algoliasearch/check_synonym.go
@@ -0,0 +1,27 @@
+package algoliasearch
+
+import "fmt"
+
+func checkSynonym(s Synonym) error {
+	switch s := s.(type) {
+	case AltCorrectionSynonym,
+		OneWaySynonym,
+		PlaceholderSynonym,
+		SimpleSynonym:
+		// Type is valid. Do nothing.
+	default:
+		return invalidParameter(fmt.Sprintf("%T", s))
+	}
+
+	return nil
+}
+
+func checkSynonyms(synonyms []Synonym) error {
+	for _, s := range synonyms {
+		if err := checkSynonym(s); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
