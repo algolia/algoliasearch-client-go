63b831702e5c4f7c4b8c1b3d5439573b1817fe9b
Anthony Seure <anthony.seure@gmail.com>
2016-05-13 17:58:22 +0200

    Simplify `Client.MultipleQueries`

--- algoliasearch/types_query.go
+++ algoliasearch/types_query.go
@@ -10,13 +10,18 @@ type MultipleQueryRes struct {
 }
 
 type QueryRes struct {
-	Hits             []Map `json:"hits"`
-	HitsPerPage      int                      `json:"hitsPerPage"`
-	NbHits           int                      `json:"nbHits"`
-	NbPages          int                      `json:"nbPages"`
-	Page             int                      `json:"page"`
-	Map           string                   `json:"params"`
-	ParsedQuery      string                   `json:"parsedQuery,omitempty"`
-	ProcessingTimeMS int                      `json:"processingTimeMS"`
-	Query            string                   `json:"query"`
+	Hits             []Map  `json:"hits"`
+	HitsPerPage      int    `json:"hitsPerPage"`
+	NbHits           int    `json:"nbHits"`
+	NbPages          int    `json:"nbPages"`
+	Page             int    `json:"page"`
+	Map              string `json:"params"`
+	ParsedQuery      string `json:"parsedQuery,omitempty"`
+	ProcessingTimeMS int    `json:"processingTimeMS"`
+	Query            string `json:"query"`
+}
+
+type IndexedQuery struct {
+	IndexName string
+	Params    Map
 }
