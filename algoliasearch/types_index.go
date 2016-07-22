c651f16e0c6bd6bddc4d0ec9df60d3df15a24a9c
Anthony Seure <anthony.seure@gmail.com>
2016-07-22 10:39:14 +0200

    Add two missing fields to IndexRes struct
    The following fields were missing from the IndexRes struct and have been added:
      - FileSize (integer)
      - NumberOfPendingTask (integer)

--- algoliasearch/types_index.go
+++ algoliasearch/types_index.go
@@ -1,13 +1,15 @@
 package algoliasearch
 
 type IndexRes struct {
-	CreatedAt      string `json:"createdAt"`
-	DataSize       int    `json:"dataSize"`
-	Entries        int    `json:"entries"`
-	LastBuildTimeS int    `json:"lastBuildTimeS"`
-	Name           string `json:"name"`
-	PendingTask    bool   `json:"pendingTask"`
-	UpdatedAt      string `json:"updatedAt"`
+	CreatedAt           string `json:"createdAt"`
+	DataSize            int    `json:"dataSize"`
+	Entries             int    `json:"entries"`
+	FileSize            int    `json:"fileSize"`
+	LastBuildTimeS      int    `json:"lastBuildTimeS"`
+	Name                string `json:"name"`
+	NumberOfPendingTask int    `json:"numberOfPendingTask"`
+	PendingTask         bool   `json:"pendingTask"`
+	UpdatedAt           string `json:"updatedAt"`
 }
 
 type listIndexesRes struct {
