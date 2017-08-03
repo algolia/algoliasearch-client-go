01162b2ae266675d179095a628480a85c0de8002
Anthony Seure <anthony.seure@gmail.com>
2017-08-03 10:09:14 +0200

    feat: Implement request options handling (client)

--- algoliasearch/client.go
+++ algoliasearch/client.go
@@ -44,9 +44,13 @@ func (c *client) SetHTTPClient(client *http.Client) {
 }
 
 func (c *client) ListIndexes() (indexes []IndexRes, err error) {
+	return c.ListIndexesWithRequestOptions(nil)
+}
+
+func (c *client) ListIndexesWithRequestOptions(opts *RequestOptions) (indexes []IndexRes, err error) {
 	var res listIndexesRes
 
-	err = c.request(&res, "GET", "/1/indexes", nil, read)
+	err = c.request(&res, "GET", "/1/indexes", nil, read, opts)
 	indexes = res.Items
 	return
 }
@@ -56,31 +60,50 @@ func (c *client) InitIndex(name string) Index {
 }
 
 func (c *client) ListKeys() (keys []Key, err error) {
-	var res listKeysRes
+	return c.ListKeysWithRequestOptions(nil)
+}
 
-	err = c.request(&res, "GET", "/1/keys", nil, read)
+func (c *client) ListKeysWithRequestOptions(opts *RequestOptions) (keys []Key, err error) {
+	var res listKeysRes
+	err = c.request(&res, "GET", "/1/keys", nil, read, opts)
 	keys = res.Keys
 	return
 }
 
 func (c *client) MoveIndex(source, destination string) (UpdateTaskRes, error) {
+	return c.MoveIndexWithRequestOptions(source, destination, nil)
+}
+
+func (c *client) MoveIndexWithRequestOptions(source, destination string, opts *RequestOptions) (UpdateTaskRes, error) {
 	index := c.InitIndex(source)
-	return index.Move(destination)
+	return index.MoveWithRequestOptions(destination, opts)
 }
 
 func (c *client) CopyIndex(source, destination string) (UpdateTaskRes, error) {
+	return c.CopyIndexWithRequestOptions(source, destination, nil)
+}
+
+func (c *client) CopyIndexWithRequestOptions(source, destination string, opts *RequestOptions) (UpdateTaskRes, error) {
 	index := c.InitIndex(source)
-	return index.Copy(destination)
+	return index.CopyWithRequestOptions(destination, opts)
 }
 
 func (c *client) DeleteIndex(name string) (res DeleteTaskRes, err error) {
+	return c.DeleteIndexWithRequestOptions(name, nil)
+}
+
+func (c *client) DeleteIndexWithRequestOptions(name string, opts *RequestOptions) (res DeleteTaskRes, err error) {
 	index := c.InitIndex(name)
-	return index.Delete()
+	return index.DeleteWithRequestOptions(opts)
 }
 
 func (c *client) ClearIndex(name string) (res UpdateTaskRes, err error) {
+	return c.ClearIndexWithRequestOptions(name, nil)
+}
+
+func (c *client) ClearIndexWithRequestOptions(name string, opts *RequestOptions) (res UpdateTaskRes, err error) {
 	index := c.InitIndex(name)
-	return index.Clear()
+	return index.ClearWithRequestOptions(opts)
 }
 
 func (c *client) AddUserKey(ACL []string, params Map) (AddKeyRes, error) {
@@ -88,6 +111,10 @@ func (c *client) AddUserKey(ACL []string, params Map) (AddKeyRes, error) {
 }
 
 func (c *client) AddAPIKey(ACL []string, params Map) (res AddKeyRes, err error) {
+	return c.AddAPIKeyWithRequestOptions(ACL, params, nil)
+}
+
+func (c *client) AddAPIKeyWithRequestOptions(ACL []string, params Map, opts *RequestOptions) (res AddKeyRes, err error) {
 	req := duplicateMap(params)
 	req["acl"] = ACL
 
@@ -95,7 +122,7 @@ func (c *client) AddAPIKey(ACL []string, params Map) (res AddKeyRes, err error) 
 		return
 	}
 
-	err = c.request(&res, "POST", "/1/keys/", req, read)
+	err = c.request(&res, "POST", "/1/keys/", req, read, opts)
 	return
 }
 
@@ -104,12 +131,16 @@ func (c *client) UpdateUserKey(key string, params Map) (UpdateKeyRes, error) {
 }
 
 func (c *client) UpdateAPIKey(key string, params Map) (res UpdateKeyRes, err error) {
+	return c.UpdateAPIKeyWithRequestOptions(key, params, nil)
+}
+
+func (c *client) UpdateAPIKeyWithRequestOptions(key string, params Map, opts *RequestOptions) (res UpdateKeyRes, err error) {
 	if err = checkKey(params); err != nil {
 		return
 	}
 
 	path := "/1/keys/" + url.QueryEscape(key)
-	err = c.request(&res, "PUT", path, params, write)
+	err = c.request(&res, "PUT", path, params, write, opts)
 	return
 }
 
@@ -118,8 +149,12 @@ func (c *client) GetUserKey(key string) (Key, error) {
 }
 
 func (c *client) GetAPIKey(key string) (res Key, err error) {
+	return c.GetAPIKeyWithRequestOptions(key, nil)
+}
+
+func (c *client) GetAPIKeyWithRequestOptions(key string, opts *RequestOptions) (res Key, err error) {
 	path := "/1/keys/" + url.QueryEscape(key)
-	err = c.request(&res, "GET", path, nil, read)
+	err = c.request(&res, "GET", path, nil, read, opts)
 	return
 }
 
@@ -128,24 +163,36 @@ func (c *client) DeleteUserKey(key string) (DeleteRes, error) {
 }
 
 func (c *client) DeleteAPIKey(key string) (res DeleteRes, err error) {
+	return c.DeleteAPIKeyWithRequestOptions(key, nil)
+}
+
+func (c *client) DeleteAPIKeyWithRequestOptions(key string, opts *RequestOptions) (res DeleteRes, err error) {
 	path := "/1/keys/" + url.QueryEscape(key)
-	err = c.request(&res, "DELETE", path, nil, write)
+	err = c.request(&res, "DELETE", path, nil, write, opts)
 	return
 }
 
 func (c *client) GetLogs(params Map) (logs []LogRes, err error) {
+	return c.GetLogsWithRequestOptions(params, nil)
+}
+
+func (c *client) GetLogsWithRequestOptions(params Map, opts *RequestOptions) (logs []LogRes, err error) {
 	var res getLogsRes
 
 	if err = checkGetLogs(params); err != nil {
 		return
 	}
 
-	err = c.request(&res, "GET", "/1/logs", params, write)
+	err = c.request(&res, "GET", "/1/logs", params, write, opts)
 	logs = res.Logs
 	return
 }
 
 func (c *client) MultipleQueries(queries []IndexedQuery, strategy string) (res []MultipleQueryRes, err error) {
+	return c.MultipleQueriesWithRequestOptions(queries, strategy, nil)
+}
+
+func (c *client) MultipleQueriesWithRequestOptions(queries []IndexedQuery, strategy string, opts *RequestOptions) (res []MultipleQueryRes, err error) {
 	if strategy == "" {
 		strategy = "none"
 	}
@@ -170,24 +217,28 @@ func (c *client) MultipleQueries(queries []IndexedQuery, strategy string) (res [
 	}
 
 	var m multipleQueriesRes
-	err = c.request(&m, "POST", "/1/indexes/*/queries", body, search)
+	err = c.request(&m, "POST", "/1/indexes/*/queries", body, search, opts)
 	res = m.Results
 	return
 }
 
 func (c *client) Batch(operations []BatchOperationIndexed) (res MultipleBatchRes, err error) {
+	return c.BatchWithRequestOptions(operations, nil)
+}
+
+func (c *client) BatchWithRequestOptions(operations []BatchOperationIndexed, opts *RequestOptions) (res MultipleBatchRes, err error) {
 	// TODO: Use check functions of index.go
 
 	request := map[string][]BatchOperationIndexed{
 		"requests": operations,
 	}
 
-	err = c.request(&res, "POST", "/1/indexes/*/batch", request, write)
+	err = c.request(&res, "POST", "/1/indexes/*/batch", request, write, opts)
 	return
 }
 
-func (c *client) request(res interface{}, method, path string, body interface{}, typeCall int) error {
-	r, err := c.transport.request(method, path, body, typeCall)
+func (c *client) request(res interface{}, method, path string, body interface{}, typeCall int, opts *RequestOptions) error {
+	r, err := c.transport.request(method, path, body, typeCall, opts)
 	if err != nil {
 		return err
 	}
