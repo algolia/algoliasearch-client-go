388b2bcab76d3248cb4eb2bb656896a95bfa08d1
Anthony Seure <anthony.seure@gmail.com>
2017-10-10 16:48:53 +0200

    feat: Implement RuleIterator to browse over all the query rules of a given index

--- algoliasearch/errors.go
+++ algoliasearch/errors.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	NoMoreHitsErr     error = errors.New("No more hits")
 	NoMoreSynonymsErr error = errors.New("No more synonyms")
+	NoMoreRulesErr    error = errors.New("No more rules")
 )
