355442608a7b3d9c8d3afcd6ee92765ed7fadb29
Anthony Seure <anthony.seure@gmail.com>
2017-10-10 16:04:54 +0200

    feat: Implement SynonymIterator to browse over all the synonyms of a given index

--- algoliasearch/errors.go
+++ algoliasearch/errors.go
@@ -3,5 +3,6 @@ package algoliasearch
 import "errors"
 
 var (
-	NoMoreHitsErr error = errors.New("No more hits")
+	NoMoreHitsErr     error = errors.New("No more hits")
+	NoMoreSynonymsErr error = errors.New("No more synonyms")
 )
