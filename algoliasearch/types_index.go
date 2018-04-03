d77dd3309baf47cae946fbe7976edd5cdcd15b5a
Anthony Seure <anthony.seure@gmail.com>
2018-04-03 12:08:03 +0200

    fix: Add missing trailing S to IndexRes' NumberOfPendingTasks (closes #386)

--- algoliasearch/types_index.go
+++ algoliasearch/types_index.go
@@ -1,15 +1,15 @@
 package algoliasearch
 
 type IndexRes struct {
-	CreatedAt           string `json:"createdAt"`
-	DataSize            int    `json:"dataSize"`
-	Entries             int    `json:"entries"`
-	FileSize            int    `json:"fileSize"`
-	LastBuildTimeS      int    `json:"lastBuildTimeS"`
-	Name                string `json:"name"`
-	NumberOfPendingTask int    `json:"numberOfPendingTask"`
-	PendingTask         bool   `json:"pendingTask"`
-	UpdatedAt           string `json:"updatedAt"`
+	CreatedAt            string `json:"createdAt"`
+	DataSize             int    `json:"dataSize"`
+	Entries              int    `json:"entries"`
+	FileSize             int    `json:"fileSize"`
+	LastBuildTimeS       int    `json:"lastBuildTimeS"`
+	Name                 string `json:"name"`
+	NumberOfPendingTasks int    `json:"numberOfPendingTasks"`
+	PendingTask          bool   `json:"pendingTask"`
+	UpdatedAt            string `json:"updatedAt"`
 }
 
 type listIndexesRes struct {
