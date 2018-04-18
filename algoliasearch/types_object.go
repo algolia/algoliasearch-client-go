cb025d79a36f4afcbe691b1ae4258f12a1fcaad7
Anthony Seure <anthony.seure@gmail.com>
2018-04-18 16:01:09 +0200

    refactor: Better describe object errors

--- algoliasearch/types_object.go
+++ algoliasearch/types_object.go
@@ -1,6 +1,9 @@
 package algoliasearch
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 type CreateObjectRes struct {
 	CreatedAt string `json:"createdAt"`
@@ -23,13 +26,21 @@ type Object Map
 func (o Object) ObjectID() (objectID string, err error) {
 	i, ok := o["objectID"]
 	if !ok {
-		err = fmt.Errorf("Cannot extract `objectID` field from Object")
+		err = fmt.Errorf("Cannot extract `objectID` field from Object `%s`", o)
 		return
 	}
 
 	if objectID, ok = i.(string); !ok {
-		err = fmt.Errorf("Cannot cast `objectID` field to string type")
+		err = fmt.Errorf("Cannot cast `objectID` field to string type from Object `%s`", o)
 	}
 
 	return
+}
+
+func (o Object) String() string {
+	jsonContent, err := json.Marshal(&o)
+	if err == nil {
+		return string(jsonContent)
+	}
+	return fmt.Sprintf("%#v", o)
 }
