21bf6744cc3f3667a6a3c719871e03afa9aa522a
Anthony Seure <anthony.seure@gmail.com>
2018-11-20 11:06:32 +0100

    changed: Use the environment variables from our Common Test Suite for testing

--- algoliasearch/secured_api_key_test.go
+++ algoliasearch/secured_api_key_test.go
@@ -3,13 +3,18 @@ package algoliasearch
 import (
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestGenerateSecuredAPIKey_Generation(t *testing.T) {
 	t.Parallel()
-	apiKey := os.Getenv("ALGOLIA_API_KEY")
+
+	apiKey := os.Getenv("ALGOLIA_SEARCH_KEY_1")
 	if apiKey == "" {
-		t.Fatal("TestGenerateSecuredAPIKey_Generation: Missing ALGOLIA_API_KEY")
+		t.Fatal("TestGenerateSecuredAPIKey_Generation: Missing ALGOLIA_SEARCH_KEY_1")
 	}
 
 	t.Log("TestGenerateSecuredAPIKey_Generation: Tests invalid key generations")
@@ -26,12 +31,7 @@ func TestGenerateSecuredAPIKey_Generation(t *testing.T) {
 
 		for _, c := range cases {
 			_, err := GenerateSecuredAPIKey(apiKey, c.params)
-			if err == nil || err.Error() != c.expectedErr.Error() {
-				t.Errorf("TestGenerateSecuredAPIKey_Generation: Calling GenerateSecuredAPIKey(%#v)\nexpected error:  \"%s\"\nbut got instead: \"%s\"",
-					c.params,
-					c.expectedErr,
-					err)
-			}
+			require.Equal(t, c.expectedErr, err, "wrong error when generating secured API key for %#v", c.params)
 		}
 	}
 
@@ -41,24 +41,16 @@ func TestGenerateSecuredAPIKey_Generation(t *testing.T) {
 			params      Map
 			expectedKey string
 		}{
-			{Map{"userToken": "user42"}, "NTc5YjBkMTgwYjdkMzBlYzllYzY5MmY3OGRmOGQzMWU3ZWU0ZTI2ZmY4MGQ0ZTZhMWZlNzJiMzllMjg5YzhmZnVzZXJUb2tlbj11c2VyNDI="},
-			{Map{"restrictIndices": "myIndex"}, "ZGQ5NjRjYTdmOWRkYzYwMjBkNWQ4ZGQ3MmZlY2RkOTYyZjIxM2FjNDBhMjBhYzhhNDFiYWI4NDE4ZGJiOTgxYXJlc3RyaWN0SW5kaWNlcz1teUluZGV4"},
-			{Map{"restrictSources": ""}, "NmNjYzQ0MzI0MmU3OTg1NDJiZDYyNTIwZjE2OWMwYjU1MjQ0ZmFhNDdmNzdjNDg1MGYxYmY1YWJjNWZkOTU2OHJlc3RyaWN0U291cmNlcz0="},
-			{Map{"validUntil": 1481901339150}, "NDZiMWNlNDMyMzEzNTRkZjFiYmMyYjE2ZWFmNzVjOWE5MjkzNTllMTgxZjM3NDI1OWNiZjAyOGZjMTc0NzU3MXZhbGlkVW50aWw9MTQ4MTkwMTMzOTE1MA=="},
+			{Map{"userToken": "user42"}, "ZDhjZmNmMmMzY2Y4MWY1NjBhMDg0NjNkYzMxYTA2ZTM5YTIyY2JkZDVmMDAwMDNhMDQ2ZGUyYjZkMjc0ZjdmOXVzZXJUb2tlbj11c2VyNDI="},
+			{Map{"restrictIndices": "myIndex"}, "OWEwZGMwZGRjYTg3YzY2NzA2MTA2YTQ2YTIyN2Q1MDk4N2NlY2Y5OTE4ZWU4Y2U5NTkyYWQ2MWI1ZTQ3NjMwOHJlc3RyaWN0SW5kaWNlcz1teUluZGV4"},
+			{Map{"restrictSources": ""}, "NWMyZjI2ZWNmMTZiY2YyZTNlZmQzZTFjZTg0MGE1OTU2ZjE1NWE2ZGQ2OWI4OTRjMmVhMzJhNDkyNzQ4MjQwN3Jlc3RyaWN0U291cmNlcz0="},
+			{Map{"validUntil": 1481901339150}, "NDFiNDFkMzNkNmQ5NDZhZTU2MmIxMzVmODg1YWZkNWI2ODIzMWE5NDIyZGJlNDRlNjM3OWMwOGJhY2RmY2I0NXZhbGlkVW50aWw9MTQ4MTkwMTMzOTE1MA=="},
 		}
 
 		for _, c := range cases {
 			generatedKey, err := GenerateSecuredAPIKey(apiKey, c.params)
-			if err != nil {
-				t.Errorf("TestGenerateSecuredAPIKey_Generation: Key with params %#v should have been generated withouth error but got: %s", c.params, err)
-			}
-
-			if generatedKey != c.expectedKey {
-				t.Errorf("TestGenerateSecuredAPIKey_Generation: Key was not generated correctly for params %#v:\nexpected key:  \"%s\"\nbut got instead: \"%s\"",
-					c.params,
-					c.expectedKey,
-					generatedKey)
-			}
+			assert.NoError(t, err, "should not error while generating secured API key for %#v", c.params)
+			assert.Equal(t, c.expectedKey, generatedKey, "wrong secured API key was generated for %#v", c.params)
 		}
 	}
 }
@@ -66,26 +58,18 @@ func TestGenerateSecuredAPIKey_Generation(t *testing.T) {
 func TestGenerateSecuredAPIKey_Usage(t *testing.T) {
 	t.Parallel()
 
-	t.Log("TestGenerateSecuredAPIKey_Usage: Obtain application ID/API key from the environment")
-	appID := os.Getenv("ALGOLIA_APPLICATION_ID")
-	if appID == "" {
-		t.Fatalf("TestGenerateSecuredAPIKey_Usage: Cannot retrieve application ID from environment")
-	}
-	apiKey := os.Getenv("ALGOLIA_SEARCH_KEY")
-	if apiKey == "" {
-		t.Fatal("TestGenerateSecuredAPIKey_Usage: Missing ALGOLIA_SEARCH_KEY")
-	}
+	appID := os.Getenv("ALGOLIA_APPLICATION_ID_1")
+	require.NotEmpty(t, appID)
 
-	t.Log("TestGenerateSecuredAPIKey_Usage: Generate secured API key")
+	apiKey := os.Getenv("ALGOLIA_SEARCH_KEY_1")
+	require.NotEmpty(t, apiKey)
+
 	params := Map{
 		"restrictIndices": "TestGenerateSecuredAPIKey_Usage_1,TestGenerateSecuredAPIKey_Usage_2",
 	}
 	key, err := GenerateSecuredAPIKey(apiKey, params)
-	if err != nil {
-		t.Fatalf("TestGenerateSecuredAPIKey_Usage: should generate key without error")
-	}
+	require.NoError(t, err)
 
-	t.Log("TestGenerateSecuredAPIKey_Usage: Check if key correctly restrict accesse to indices")
 	client := initClient(t)
 	restrictedClient := NewClient(appID, key)
 
@@ -102,11 +86,10 @@ func TestGenerateSecuredAPIKey_Usage(t *testing.T) {
 
 		i = restrictedClient.InitIndex(c.indexName)
 		_, err := i.Search("", nil)
-		if c.isAuthorized && err != nil {
-			t.Fatalf("TestGenerateSecuredAPIKey_Usage: index %s should be searchable with restricted API key", c.indexName)
-		}
-		if !c.isAuthorized && err == nil {
-			t.Fatalf("TestGenerateSecuredAPIKey_Usage: index %s should not be searchable with restricted API key", c.indexName)
+		if c.isAuthorized {
+			assert.NoError(t, err, "should be able to search in index %q", c.indexName)
+		} else {
+			assert.Error(t, err, "should be able to search in index %q", c.indexName)
 		}
 	}
 }
