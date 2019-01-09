c96bcea9dc69b840ca87ca5cf803fe2f89ee8fca
Anthony Seure <anthony.seure@gmail.com>
2019-01-09 15:49:04 +0100

    changed: introduce opt.RequestOptions function to avoid exposing a raw structure

--- algolia/opt/request_options.go
+++ algolia/opt/request_options.go
@@ -1,16 +1,34 @@
 package opt
 
-type RequestOptions struct {
+type requestOptions struct {
 	ExtraHeaders   map[string]string
 	ExtraURLParams map[string]string
 }
 
-func ExtractRequestOptions(opts ...interface{}) (options RequestOptions) {
+func newRequestOptions() requestOptions {
+	return requestOptions{
+		ExtraHeaders:   make(map[string]string),
+		ExtraURLParams: make(map[string]string),
+	}
+}
+
+func RequestOptions(extraHeaders, extraURLParams map[string]string) requestOptions {
+	opts := newRequestOptions()
+	if extraHeaders != nil {
+		opts.ExtraHeaders = extraHeaders
+	}
+	if extraURLParams != nil {
+		opts.ExtraURLParams = extraURLParams
+	}
+	return opts
+}
+
+func ExtractRequestOptions(opts ...interface{}) (options requestOptions) {
 	for _, opt := range opts {
-		v, ok := opt.(RequestOptions)
+		v, ok := opt.(requestOptions)
 		if ok {
 			return v
 		}
 	}
-	return RequestOptions{}
+	return requestOptions{}
 }
