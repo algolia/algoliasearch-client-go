1c1b96a6e1789e8c086288c6cf6568f4389b0068
Anthony Seure <anthony.seure@gmail.com>
2019-03-08 17:57:55 +0100

    changed: do not generate RuleIterator anymore (as implementation is not the same as SynonymIterator)

--- algolia/internal/gen/gen.go
+++ algolia/internal/gen/gen.go
@@ -4,4 +4,3 @@ package gen
 //go:generate go run gen_extract.go       options.go functions.go
 //go:generate go run gen_settings.go      options.go functions.go
 //go:generate go run gen_search_params.go options.go functions.go
-//go:generate go run gen_iterator.go      options.go functions.go
