9b95821bfeeaa3e6beeb8dca348009b14ea09ee9
Anthony Seure <anthony.seure@gmail.com>
2019-03-12 15:14:29 +0100

    changed: remove useless type hints for common exported errors

--- algolia/search/errors.go
+++ algolia/search/errors.go
@@ -3,8 +3,8 @@ package search
 import "errors"
 
 var (
-	NoMoreHitsErr     error = errors.New("No more hits")
-	NoMoreRulesErr    error = errors.New("No more rules")
-	NoMoreSynonymsErr error = errors.New("No more synonyms")
+	NoMoreHitsErr     = errors.New("No more hits")
+	NoMoreRulesErr    = errors.New("No more rules")
+	NoMoreSynonymsErr = errors.New("No more synonyms")
 	// TODO: centralize most errors into this file
 )
