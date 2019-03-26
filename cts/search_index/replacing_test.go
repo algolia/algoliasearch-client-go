8048f8c734c889450f16c9e1d7acca3272d36ba8
Anthony Seure <anthony.seure@gmail.com>
2019-03-26 14:13:38 +0100

    changed: migrate CTS test for replacement methods

---
+++ cts/search_index/replacing_test.go
@@ -0,0 +1,108 @@
+package search_index
+
+import (
+	"testing"
+
+	"github.com/algolia/algoliasearch-client-go/algolia"
+	"github.com/algolia/algoliasearch-client-go/algolia/search"
+	"github.com/algolia/algoliasearch-client-go/cts"
+	"github.com/stretchr/testify/require"
+)
+
+func TestReplacing(t *testing.T) {
+	t.Parallel()
+	_, index, _ := cts.InitSearchClient1AndIndex(t)
+
+	await := algolia.Await()
+
+	{
+		res, err := index.SaveObject(map[string]string{"objectID": "one"})
+		require.NoError(t, err)
+		await.Collect(res)
+	}
+
+	{
+		res, err := index.SaveRule(search.Rule{
+			ObjectID:  "one",
+			Condition: search.RuleCondition{Anchoring: search.Contains, Pattern: "pattern"},
+			Consequence: search.RuleConsequence{
+				Params: &search.RuleParams{
+					Query: search.NewRuleQueryObject(
+						search.RuleQueryObjectQuery{
+							Edits: []search.QueryEdit{
+								search.RemoveEdit("pattern"),
+							},
+						},
+					),
+				},
+			},
+		})
+		require.NoError(t, err)
+		await.Collect(res)
+	}
+
+	{
+		res, err := index.SaveSynonym(search.NewRegularSynonym("one", "one", "two"))
+		require.NoError(t, err)
+		await.Collect(res)
+	}
+
+	require.NoError(t, await.Wait())
+
+	{
+		err := index.ReplaceAllObjects([]map[string]string{{"objectID": "two"}})
+		require.NoError(t, err)
+	}
+
+	{
+		res, err := index.ReplaceAllRules([]search.Rule{
+			{
+				ObjectID:  "two",
+				Condition: search.RuleCondition{Anchoring: search.Contains, Pattern: "pattern"},
+				Consequence: search.RuleConsequence{
+					Params: &search.RuleParams{
+						Query: search.NewRuleQueryObject(
+							search.RuleQueryObjectQuery{
+								Edits: []search.QueryEdit{
+									search.RemoveEdit("pattern"),
+								},
+							},
+						),
+					},
+				},
+			},
+		})
+		require.NoError(t, err)
+		await.Collect(res)
+	}
+
+	{
+		res, err := index.ReplaceAllSynonyms([]search.Synonym{
+			search.NewRegularSynonym("two", "one", "two"),
+		})
+		require.NoError(t, err)
+		await.Collect(res)
+	}
+
+	require.NoError(t, await.Wait())
+
+	{
+		err := index.GetObject("one", nil)
+		require.Error(t, err)
+
+		err = index.GetObject("two", nil)
+		require.NoError(t, err)
+
+		_, err = index.GetRule("one")
+		require.Error(t, err)
+
+		_, err = index.GetRule("two")
+		require.NoError(t, err)
+
+		_, err = index.GetSynonym("one")
+		require.Error(t, err)
+
+		_, err = index.GetSynonym("two")
+		require.NoError(t, err)
+	}
+}
