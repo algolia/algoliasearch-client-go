862c1e0926c58338e8782fbfbbeba0dbe44fe27f
Anthony Seure <anthony.seure@gmail.com>
2019-04-03 11:00:22 +0200

    changed: simplify deserialization of IndexRes type for search.Client.ListIndexes

--- algolia/search/responses_list_indexes.go
+++ algolia/search/responses_list_indexes.go
@@ -11,15 +11,15 @@ type ListIndexesRes struct {
 }
 
 type IndexRes struct {
-	CreatedAt            time.Time
-	DataSize             int64
-	Entries              int64
-	FileSize             int64
-	LastBuildTime        time.Duration
-	Name                 string
-	NumberOfPendingTasks int64
-	PendingTask          bool
-	UpdatedAt            time.Time
+	CreatedAt            time.Time     `json:"createdAt"`
+	DataSize             int64         `json:"dataSize"`
+	Entries              int64         `json:"entries"`
+	FileSize             int64         `json:"fileSize"`
+	LastBuildTime        time.Duration `json:"-"`
+	Name                 string        `json:"name"`
+	NumberOfPendingTasks int64         `json:"numberOfPendingTasks"`
+	PendingTask          bool          `json:"pendingTask"`
+	UpdatedAt            time.Time     `json:"updatedAt"`
 }
 
 func (r *IndexRes) UnmarshalJSON(data []byte) error {
@@ -27,34 +27,16 @@ func (r *IndexRes) UnmarshalJSON(data []byte) error {
 		return nil
 	}
 
-	type indexRes struct {
-		IndexRes
-		LastBuildTimeS int `json:"lastBuildTimeS"`
-	}
-
+	type indexRes IndexRes
 	var res struct {
-		CreatedAt            time.Time `json:"createdAt"`
-		DataSize             int64     `json:"dataSize"`
-		Entries              int64     `json:"entries"`
-		FileSize             int64     `json:"fileSize"`
-		LastBuildTimeS       int64     `json:"lastBuildTimeS"`
-		Name                 string    `json:"name"`
-		NumberOfPendingTasks int64     `json:"numberOfPendingTasks"`
-		PendingTask          bool      `json:"pendingTask"`
-		UpdatedAt            time.Time `json:"updatedAt"`
+		LastBuildTimeS int64 `json:"lastBuildTimeS"`
+		indexRes
 	}
 	err := json.Unmarshal(data, &res)
 	if err != nil {
 		return err
 	}
-	r.CreatedAt = res.CreatedAt
-	r.DataSize = res.DataSize
-	r.Entries = res.Entries
-	r.FileSize = res.FileSize
+	*r = IndexRes(res.indexRes)
 	r.LastBuildTime = time.Duration(res.LastBuildTimeS) * time.Second
-	r.Name = res.Name
-	r.NumberOfPendingTasks = res.NumberOfPendingTasks
-	r.PendingTask = res.PendingTask
-	r.UpdatedAt = res.UpdatedAt
 	return nil
 }
