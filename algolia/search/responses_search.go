0c8f82b73e5e28b88ced6daa774186e8d05cd569
Anthony Seure <anthony.seure@gmail.com>
2019-04-09 17:12:39 +0200

    fixed: improve QueryRes by adding and correctly typing fields

--- algolia/search/responses_search.go
+++ algolia/search/responses_search.go
@@ -7,38 +7,46 @@ import (
 )
 
 type QueryRes struct {
-	AppliedRules          interface{}              `json:"appliedRules"` // TODO to type correctly or add an unmarshalling method
-	AroundLatLng          string                   `json:"aroundLatLng"`
-	AutomaticRadius       string                   `json:"automaticRadius"`
-	ExhaustiveFacetsCount bool                     `json:"exhaustiveFacetsCount"`
-	ExhaustiveNbHits      bool                     `json:"exhaustiveNbHits"`
-	Explain               interface{}              `json:"explain"`      // TODO to type correctly or add an unmarshalling method
-	Facets                interface{}              `json:"facets"`       // TODO to type correctly or add an unmarshalling method
-	FacetsStats           interface{}              `json:"facets_stats"` // TODO to type correctly or add an unmarshalling method
-	Hits                  []map[string]interface{} `json:"Hits"`
-	HitsPerPage           int                      `json:"hitsPerPage"`
-	Index                 string                   `json:"index"`
-	IndexUsed             string                   `json:"indexUsed"`
-	Length                int                      `json:"length"`
-	Message               string                   `json:"message"`
-	NbHits                int                      `json:"nbHits"`
-	NbPages               int                      `json:"nbPages"`
-	Offset                int                      `json:"offset"`
-	Page                  int                      `json:"page"`
-	Params                string                   `json:"params"`
-	ParsedQuery           string                   `json:"parsedQuery"`
-	ProcessingTimeMS      int                      `json:"processingTimeMS"`
-	Query                 string                   `json:"query"`
-	QueryAfterRemoval     string                   `json:"queryAfterRemoval"`
-	QueryID               string                   `json:"queryID"`
-	ServerUsed            string                   `json:"serverUsed"`
-	TimeoutCounts         bool                     `json:"timeoutCounts"`
-	TimeoutHits           bool                     `json:"timeoutHits"`
-	UserData              interface{}              `json:"userData"` // TODO to type correctly or add an unmarshalling method
-	// TODO: add and type `abTestVariantID` field
-	// TODO: add and type `message` field
-	// TODO: add and type `warning` field
-	// TODO: add and type `cursor` field
+	AppliedRules          []AppliedRule                     `json:"appliedRules"`
+	AroundLatLng          string                            `json:"aroundLatLng"`
+	AutomaticRadius       string                            `json:"automaticRadius"`
+	ExhaustiveFacetsCount bool                              `json:"exhaustiveFacetsCount"`
+	ExhaustiveNbHits      bool                              `json:"exhaustiveNbHits"`
+	Explain               map[string]map[string]interface{} `json:"explain"`
+	Facets                map[string]map[string]int         `json:"facets"`
+	FacetsStats           map[string]FacetStat              `json:"facets_stats"`
+	Hits                  []map[string]interface{}          `json:"Hits"`
+	HitsPerPage           int                               `json:"hitsPerPage"`
+	Index                 string                            `json:"index"`
+	IndexUsed             string                            `json:"indexUsed"`
+	Length                int                               `json:"length"`
+	Message               string                            `json:"message"`
+	NbHits                int                               `json:"nbHits"`
+	NbPages               int                               `json:"nbPages"`
+	Offset                int                               `json:"offset"`
+	Page                  int                               `json:"page"`
+	Params                string                            `json:"params"`
+	ParsedQuery           string                            `json:"parsedQuery"`
+	ProcessingTimeMS      int                               `json:"processingTimeMS"`
+	Query                 string                            `json:"query"`
+	QueryAfterRemoval     string                            `json:"queryAfterRemoval"`
+	QueryID               string                            `json:"queryID"`
+	ServerUsed            string                            `json:"serverUsed"`
+	TimeoutCounts         bool                              `json:"timeoutCounts"`
+	TimeoutHits           bool                              `json:"timeoutHits"`
+	UserData              map[string]interface{}            `json:"userData"`
+	ABTestVariantID       string                            `json:"abTestVariantID"`
+}
+
+type AppliedRule struct {
+	ObjectID string `json:"objectID"`
+}
+
+type FacetStat struct {
+	Min float64 `json:"min"`
+	Max float64 `json:"max"`
+	Avg float64 `json:"avg"`
+	Sum float64 `json:"sum"`
 }
 
 type HighlightResult map[string]HighlightedResult
