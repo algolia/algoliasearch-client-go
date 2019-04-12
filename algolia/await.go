79b4d2d6ebc940ab0325c637d083704975574e42
Anthony Seure <anthony.seure@gmail.com>
2019-04-12 17:41:18 +0200

    doc: Add documentation comment for algolia.Waitable related code

--- algolia/await.go
+++ algolia/await.go
@@ -4,6 +4,10 @@ import (
 	"sync"
 )
 
+// Waitable represents any asynchronous and potentially long-blocking operation
+// which may eventually fail. Most of Algolia response objects are implementing
+// the `algolia.Waitable` interface in order to wait for their completion using
+// `algolia.Await()` or `algolia.Wait()` provided helpers.
 type Waitable interface {
 	Wait() error
 }
@@ -13,22 +17,39 @@ type await struct {
 	waitables []Waitable
 }
 
+// Await returns a newly instantiated `await` object. This instance can collect
+// objects implementing the `algolia.Waitable` interface, including most of the
+// Algolia response objects, and wait for their completion in a concurrent
+// fashion.
 func Await() *await {
 	return new(await)
 }
 
+// Wait blocks until all the given `algolia.Waitable` objects have completed. If
+// one of the objects returned an error upon `Wait` invocation, this error is
+// returned. Otherwise, nil is returned.
 func Wait(waitables ...Waitable) error {
 	await := Await()
 	await.Collect(waitables...)
 	return await.Wait()
 }
 
+// Collect holds references to the given `algolia.Waitable` objects in order to
+// wait for their completion once the `Wait` method will be invoked. Calling
+// `Collect` from multiple goroutines is safe.
 func (a *await) Collect(waitables ...Waitable) {
 	a.Lock()
 	a.waitables = append(a.waitables, waitables...)
 	a.Unlock()
 }
 
+// Wait blocks until all the collected `algolia.Waitable` objects have
+// completed. If one of the objects returned an error upon `Wait` invocation,
+// this error is returned. Otherwise, nil is returned. Calling `Wait` from
+// multiple goroutines is safe.
+//
+// Upon successful completion, the `await` object can be reused directly to
+// collect other `algolia.Waitable` objects.
 func (a *await) Wait() error {
 	a.Lock()
 	defer a.Unlock()
