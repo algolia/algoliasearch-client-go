da54f816fc368d9841148408a95fcb3a5957fe48
Anthony Seure <anthony.seure@gmail.com>
2019-04-17 08:41:27 +0200

    changed: use lower case strings for errors as recommended by linters

--- algolia/search/errors.go
+++ algolia/search/errors.go
@@ -3,9 +3,9 @@ package search
 import "errors"
 
 var (
-	NoMoreHitsErr         = errors.New("No more hits")
-	NoMoreRulesErr        = errors.New("No more rules")
-	NoMoreSynonymsErr     = errors.New("No more synonyms")
-	SameAppIDErr          = errors.New("Indices cannot target the same application ID. Please use Client.CopyIndex for same-app index copy instead.")
-	IndexAlreadyExistsErr = errors.New("Destination index already exists. Please delete it first as the CopyIndex cannot hold the responsibility of modifying the destination index.")
+	NoMoreHitsErr         = errors.New("no more hits")
+	NoMoreRulesErr        = errors.New("no more rules")
+	NoMoreSynonymsErr     = errors.New("no more synonyms")
+	SameAppIDErr          = errors.New("indices cannot target the same application ID, please use Client.CopyIndex for same-app index copy instead")
+	IndexAlreadyExistsErr = errors.New("destination index already exists, please delete it first as the CopyIndex cannot hold the responsibility of modifying the destination index")
 )
