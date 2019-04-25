b0eb2a1ab9cc6e90384e4c69a50cdce88adee2f6
Anthony Seure <anthony.seure@gmail.com>
2019-04-25 09:19:15 +0200

    feat: add ClientInterface and IndexInterface

---
+++ algolia/search/client_interface.go
@@ -0,0 +1,48 @@
+package search
+
+type ClientInterface interface {
+	// Misc
+	InitIndex(indexName string) *Index
+	ListIndexes(opts ...interface{}) (res ListIndexesRes, err error)
+	GetLogs(opts ...interface{}) (res GetLogsRes, err error)
+
+	// Copy index operations
+	CopyRules(source, destination string, opts ...interface{}) (UpdateTaskRes, error)
+	CopySettings(source, destination string, opts ...interface{}) (UpdateTaskRes, error)
+	CopySynonyms(source, destination string, opts ...interface{}) (UpdateTaskRes, error)
+	CopyIndex(source, destination string, opts ...interface{}) (UpdateTaskRes, error)
+
+	// Move index operations
+	MoveRules(source, destination string, opts ...interface{}) (UpdateTaskRes, error)
+	MoveSettings(source, destination string, opts ...interface{}) (UpdateTaskRes, error)
+	MoveSynonyms(source, destination string, opts ...interface{}) (UpdateTaskRes, error)
+	MoveIndex(source, destination string, opts ...interface{}) (UpdateTaskRes, error)
+
+	// API key methods
+	GetAPIKey(keyID string, opts ...interface{}) (key Key, err error)
+	AddAPIKey(key Key, opts ...interface{}) (res CreateKeyRes, err error)
+	UpdateAPIKey(key Key, opts ...interface{}) (res UpdateKeyRes, err error)
+	DeleteAPIKey(keyID string, opts ...interface{}) (res DeleteKeyRes, err error)
+	RestoreAPIKey(keyID string, opts ...interface{}) (res RestoreKeyRes, err error)
+	ListAPIKeys(opts ...interface{}) (res ListAPIKeysRes, err error)
+
+	// Multiple methods
+	MultipleBatch(operations []BatchOperationIndexed, opts ...interface{}) (res MultipleBatchRes, err error)
+	MultipleGetObjects(requests []IndexedGetObject, objects interface{}, opts ...interface{}) (err error)
+	MultipleQueries(queries []IndexedQuery, strategy string, opts ...interface{}) (res MultipleQueriesRes, err error)
+
+	// Multi-Cluster Management (MCM) methods
+	ListClusters(opts ...interface{}) (res ListClustersRes, err error)
+	ListUserIDs(opts ...interface{}) (res ListUserIDsRes, err error)
+	GetUserID(userID string, opts ...interface{}) (res UserID, err error)
+	AssignUserID(userID, clusterName string, opts ...interface{}) (res AssignUserIDRes, err error)
+	RemoveUserID(userID string, opts ...interface{}) (res RemoveUserIDRes, err error)
+	GetTopUserIDs(opts ...interface{}) (res TopUserIDs, err error)
+	SearchUserIDs(query string, opts ...interface{}) (res SearchUserIDRes, err error)
+
+	// Personalization
+	SetPersonalizationStrategy(strategy Strategy, opts ...interface{}) (res SetPersonalizationStrategyRes, err error)
+	GetPersonalizationStrategy(opts ...interface{}) (res GetPersonalizationStrategyRes, err error)
+}
+
+var _ ClientInterface = &Client{}
---
+++ algolia/search/index_interface.go
@@ -0,0 +1,58 @@
+package search
+
+type IndexInterface interface {
+	// Misc
+	WaitTask(taskID int) error
+	GetStatus(taskID int) (res TaskStatusRes, err error)
+	GetAppID() string
+	Clear(opts ...interface{}) (res UpdateTaskRes, err error)
+	Delete(opts ...interface{}) (res DeleteTaskRes, err error)
+
+	// Indexing
+	GetObject(objectID string, object interface{}, opts ...interface{}) error
+	GetObjects(objectIDs []string, objects interface{}, opts ...interface{}) error
+	SaveObject(object interface{}, opts ...interface{}) (res SaveObjectRes, err error)
+	SaveObjects(objects interface{}, opts ...interface{}) (res GroupBatchRes, err error)
+	PartialUpdateObject(object interface{}, opts ...interface{}) (res UpdateTaskRes, err error)
+	PartialUpdateObjects(objects interface{}, opts ...interface{}) (res GroupBatchRes, err error)
+	DeleteObject(objectID string, opts ...interface{}) (res DeleteTaskRes, err error)
+	DeleteObjects(objectIDs []string, opts ...interface{}) (res BatchRes, err error)
+	DeleteBy(opts ...interface{}) (res UpdateTaskRes, err error)
+	Batch(operations []BatchOperation, opts ...interface{}) (res BatchRes, err error)
+
+	// Query rules
+	GetRule(objectID string, opts ...interface{}) (rule Rule, err error)
+	SaveRule(rule Rule, opts ...interface{}) (res UpdateTaskRes, err error)
+	SaveRules(rules []Rule, opts ...interface{}) (res UpdateTaskRes, err error)
+	ClearRules(opts ...interface{}) (res UpdateTaskRes, err error)
+	DeleteRule(objectID string, opts ...interface{}) (res UpdateTaskRes, err error)
+
+	// Synonyms
+	GetSynonym(objectID string, opts ...interface{}) (synonym Synonym, err error)
+	SaveSynonym(synonym Synonym, opts ...interface{}) (res UpdateTaskRes, err error)
+	SaveSynonyms(synonyms []Synonym, opts ...interface{}) (res UpdateTaskRes, err error)
+	ClearSynonyms(opts ...interface{}) (res UpdateTaskRes, err error)
+	DeleteSynonym(objectID string, opts ...interface{}) (res DeleteTaskRes, err error)
+
+	// Browsing
+	BrowseObjects(opts ...interface{}) (*ObjectIterator, error)
+	BrowseRules(opts ...interface{}) (*RuleIterator, error)
+	BrowseSynonyms(opts ...interface{}) (*SynonymIterator, error)
+
+	// Replacing
+	ReplaceAllObjects(objects interface{}, opts ...interface{}) (err error)
+	ReplaceAllRules(rules []Rule, opts ...interface{}) (UpdateTaskRes, error)
+	ReplaceAllSynonyms(synonyms []Synonym, opts ...interface{}) (UpdateTaskRes, error)
+
+	// Searching
+	Search(query string, opts ...interface{}) (res QueryRes, err error)
+	SearchForFacetValues(facet, query string, opts ...interface{}) (res SearchForFacetValuesRes, err error)
+	SearchRules(query string, opts ...interface{}) (res SearchRulesRes, err error)
+	SearchSynonyms(query string, opts ...interface{}) (res SearchSynonymsRes, err error)
+
+	// Settings
+	GetSettings(opts ...interface{}) (settings Settings, err error)
+	SetSettings(settings Settings, opts ...interface{}) (res UpdateTaskRes, err error)
+}
+
+var _ IndexInterface = &Index{}
