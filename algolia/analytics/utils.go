aea999261775dea4d1ef4d72ca9554ee33ce2bd1
Marc Helbling <marc.d.helbling@gmail.com>
2019-06-14 09:29:13 +0200

    fix: update analytics regional host name template

--- algolia/analytics/utils.go
+++ algolia/analytics/utils.go
@@ -11,7 +11,7 @@ import (
 func defaultHosts(r region.Region) (hosts []*transport.StatefulHost) {
 	switch r {
 	case region.EU, region.US:
-		hosts = append(hosts, transport.NewStatefulHost(fmt.Sprintf("%s.analytics.algolia.com", r), call.IsReadWrite))
+		hosts = append(hosts, transport.NewStatefulHost(fmt.Sprintf("analytics.%s.algolia.com", r), call.IsReadWrite))
 	default:
 		hosts = append(hosts, transport.NewStatefulHost("analytics.algolia.com", call.IsReadWrite))
 	}
