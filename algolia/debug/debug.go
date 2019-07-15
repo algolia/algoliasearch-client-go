de16563740d128811fda99d81e8db2117002f874
Anthony Seure <anthony.seure@gmail.com>
2019-07-15 16:58:00 +0200

    changed(debug): correctly decode http.Request body even when gzipped
    [changelog]

--- algolia/debug/debug.go
+++ algolia/debug/debug.go
@@ -1,13 +1,9 @@
 package debug
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"net/http"
-	"strings"
+	"time"
 )
 
 var debug bool
@@ -34,14 +30,18 @@ func Display(itf interface{}) {
 	if !debug {
 		return
 	}
+	start := time.Now()
+	var msg string
 	switch v := itf.(type) {
 	case *http.Request:
-		printRequest(v)
+		msg = debugRequest(v)
 	case *http.Response:
-		printResponse(v)
+		msg = debugResponse(v)
 	default:
-		fmt.Printf("do not know how to debug-print %#v\n", v)
+		msg = fmt.Sprintf("do not know how to display %#v", v)
 	}
+	Println(msg)
+	fmt.Printf("took %s\n", time.Now().Sub(start))
 }
 
 func Println(a ...interface{}) {
@@ -54,49 +54,4 @@ func Printf(format string, a ...interface{}) {
 	}
 	msg := fmt.Sprintf(format, a...)
 	fmt.Printf("> ALGOLIA DEBUG: %s", msg)
-}
-
-func copyReadCloser(r io.ReadCloser) (io.ReadCloser, string) {
-	if r == nil {
-		return nil, ""
-	}
-	data, err := ioutil.ReadAll(r)
-	_ = r.Close()
-	if err != nil {
-		return nil, ""
-	}
-	return ioutil.NopCloser(bytes.NewReader(data)), string(data)
-}
-
-func prettyPrintJSON(input, prefix string) string {
-	var b bytes.Buffer
-	err := json.Indent(&b, []byte(input), prefix, "  ")
-	if err != nil {
-		return input
-	}
-	return strings.TrimSuffix(b.String(), "\n")
-}
-
-func printRequest(req *http.Request) {
-	var body string
-	req.Body, body = copyReadCloser(req.Body)
-	body = prettyPrintJSON(body, "\t")
-	fmt.Printf("> ALGOLIA DEBUG request:\n\tmethod=%q\n\turl=%q\n", req.Method, req.URL)
-	for k, v := range req.Header {
-		str := strings.Join(v, ",")
-		if strings.Contains(strings.ToLower(k), "algolia") {
-			str = strings.Repeat("*", len(str))
-		}
-		fmt.Printf("\theader=%s:%q\n", k, str)
-	}
-	fmt.Printf("\tbody=\n\t%s\n", body)
-}
-
-func printResponse(res *http.Response) {
-	var body string
-	if res != nil {
-		res.Body, body = copyReadCloser(res.Body)
-	}
-	body = prettyPrintJSON(body, "\t")
-	fmt.Printf("> ALGOLIA DEBUG response:\n\tbody=\n\t%s\n", body)
 }
