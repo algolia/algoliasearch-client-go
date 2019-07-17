567171dcb87041f3f045af5a4f3ef8eacbd4b904
Anthony Seure <anthony.seure@gmail.com>
2019-07-17 10:45:36 +0200

    test: add more cases to transport.shouldCompress test

--- algolia/transport/transport_test.go
+++ algolia/transport/transport_test.go
@@ -10,23 +10,27 @@ import (
 )
 
 func TestShouldCompress(t *testing.T) {
+	fakeBody := map[string]interface{}{}
 	for _, c := range []struct {
 		compression compression.Compression
 		method      string
+		body        interface{}
 		expected    bool
 	}{
-		{0, "", false},
-		{compression.None, "", false},
-		{compression.None, http.MethodPost, false},
-		{compression.None, http.MethodPut, false},
-		{compression.None, http.MethodDelete, false},
-		{compression.None, http.MethodGet, false},
-		{compression.GZIP, http.MethodPost, true},
-		{compression.GZIP, http.MethodPut, true},
-		{compression.GZIP, http.MethodDelete, false},
-		{compression.GZIP, http.MethodGet, false},
+		{0, "", nil, false},
+		{compression.None, "", nil, false},
+		{compression.None, http.MethodPost, fakeBody, false},
+		{compression.None, http.MethodPut, fakeBody, false},
+		{compression.None, http.MethodDelete, fakeBody, false},
+		{compression.None, http.MethodGet, fakeBody, false},
+		{compression.GZIP, http.MethodPost, fakeBody, true},
+		{compression.GZIP, http.MethodPut, fakeBody, true},
+		{compression.GZIP, http.MethodDelete, fakeBody, false},
+		{compression.GZIP, http.MethodGet, fakeBody, false},
+		{compression.GZIP, http.MethodPost, nil, false},
+		{compression.GZIP, http.MethodPut, nil, false},
 	} {
-		got := shouldCompress(c.compression, c.method)
+		got := shouldCompress(c.compression, c.method, c.body)
 		require.Equal(t, c.expected, got, "compression=%d method=%q", c.compression, c.method)
 	}
 }
