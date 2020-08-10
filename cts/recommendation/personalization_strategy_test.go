4d3d0721d46e7a814dde887653177e48af054732
Anthony Seure <anthony.seure@gmail.com>
2020-08-10 13:52:03 +0200

    test: prevent error in CTS personalization test on 429 HTTP code (close #620)

--- cts/recommendation/personalization_strategy_test.go
+++ cts/recommendation/personalization_strategy_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	"github.com/algolia/algoliasearch-client-go/v3/algolia/errs"
 	"github.com/algolia/algoliasearch-client-go/v3/algolia/opt"
 	"github.com/algolia/algoliasearch-client-go/v3/algolia/recommendation"
 	"github.com/algolia/algoliasearch-client-go/v3/cts"
@@ -26,7 +27,12 @@ func TestPersonalizationStrategy(t *testing.T) {
 	}
 
 	_, err := client.SetPersonalizationStrategy(strategy)
-	require.NoError(t, err)
+	if algoliaErr, ok := errs.IsAlgoliaErr(err); ok {
+		require.Equal(t, 429, algoliaErr.Status)
+		require.Equal(t, "Number of strategy saves exceeded for the day", algoliaErr.Message)
+	} else {
+		require.NoError(t, err)
+	}
 
 	got, err := client.GetPersonalizationStrategy()
 	require.NoError(t, err)
