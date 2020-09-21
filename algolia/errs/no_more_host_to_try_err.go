f8dbf70837eca55b692b39bdf4b85fc494c1c63e
Anthony Seure <anthony.seure@gmail.com>
2020-09-21 14:40:32 +0200

    feat(transport): improve errs.NoMoreHostToTry error message to help investigations

--- algolia/errs/no_more_host_to_try_err.go
+++ algolia/errs/no_more_host_to_try_err.go
@@ -15,5 +15,5 @@ func (e *NoMoreHostToTryErr) IntermediateNetworkErrors() []error {
 }
 
 func (e *NoMoreHostToTryErr) Error() string {
-	return "all hosts have been contacted unsuccessfully, it can either be a network error or wrong appID/key credentials were used"
+	return "all hosts have been contacted unsuccessfully, it can either be a server or a network error or wrong appID/key credentials were used. You can use opt.ExposeIntermediateNetworkErrors(true) to investigate."
 }
