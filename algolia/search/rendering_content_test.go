e9c53a70acadc255665e8c908bc791ee5cdd5a25
Vladislav Fitc <vladislav.fitc@algolia.com>
2021-07-09 10:44:02 +0200

    add the rendering content field to settings

--- algolia/search/rendering_content_test.go
+++ algolia/search/rendering_content_test.go
@@ -34,3 +34,21 @@ func TestUnmarshalRenderingContent(t *testing.T) {
 	require.Equal(t, r.FacetOrdering.Values["color"].Order, []string{"red", "green"})
 	require.Nil(t, r.FacetOrdering.Values["color"].SortRemainingBy)
 }
+
+func TestBuildRenderingContent(t *testing.T) {
+
+	var r = RenderingContent{FacetOrdering: &FacetOrdering{
+		Facets: &FacetsOrder{Order: []string{"size", "brand"}},
+		Values: map[string]FacetValuesOrder{
+			"brand": NewFacetValuesOrder([]string{"Uniqlo"}, Count),
+			"size":  NewFacetValuesOrder([]string{"S", "M", "L"}, Hidden),
+		},
+	}}
+
+	require.Equal(t, r.FacetOrdering.Facets.Order, []string{"size", "brand"})
+	require.Equal(t, r.FacetOrdering.Values["brand"].Order, []string{"Uniqlo"})
+	require.Equal(t, *r.FacetOrdering.Values["brand"].SortRemainingBy, Count)
+	require.Equal(t, r.FacetOrdering.Values["size"].Order, []string{"S", "M", "L"})
+	require.Equal(t, *r.FacetOrdering.Values["size"].SortRemainingBy, Hidden)
+
+}
