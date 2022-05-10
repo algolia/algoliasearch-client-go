48a6119207897114c4fb72a9978c0591c9ae12bb
Clément Vannicatte <vannicattec@gmail.com>
2022-05-10 13:06:39 +0200

    fix(javascript): move logic to custom gens (#486)

--- generators/src/main/java/com/algolia/codegen/AlgoliaJavascriptGenerator.java
+++ generators/src/main/java/com/algolia/codegen/AlgoliaJavascriptGenerator.java
@@ -81,6 +81,40 @@ public class AlgoliaJavascriptGenerator extends TypeScriptNodeClientCodegen {
       additionalProperties
     );
 
+    List<CodegenOperation> operations =
+      ((Map<String, List<CodegenOperation>>) results.get("operations")).get(
+          "operation"
+        );
+
+    // We read operations and detect if we should wrap parameters under an object.
+    // We only wrap if there is a mix between body parameters and other parameters.
+    for (CodegenOperation ope : operations) {
+      // Nothing to wrap as there is no parameters
+      if (!ope.hasParams) {
+        continue;
+      }
+
+      boolean hasBodyParams = !ope.bodyParams.isEmpty();
+      boolean hasHeaderParams = !ope.headerParams.isEmpty();
+      boolean hasQueryParams = !ope.queryParams.isEmpty();
+      boolean hasPathParams = !ope.pathParams.isEmpty();
+
+      // If there is nothing but body params, we just check if it's a single param
+      if (
+        hasBodyParams && !hasHeaderParams && !hasQueryParams && !hasPathParams
+      ) {
+        // At this point the single parameter is already an object, to avoid double wrapping
+        // we skip it
+        if (ope.bodyParams.size() == 1 && !ope.bodyParams.get(0).isArray) {
+          ope.vendorExtensions.put("x-is-single-body-param", true);
+          continue;
+        }
+      }
+
+      // Any other cases here are wrapped
+      ope.vendorExtensions.put("x-create-wrapping-object", true);
+    }
+
     return results;
   }
 
