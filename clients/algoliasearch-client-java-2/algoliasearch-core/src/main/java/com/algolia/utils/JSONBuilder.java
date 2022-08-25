aeea736d849147e13417d7b0370bb046a2f984a6
Pierre Millot <pierre.millot@algolia.com>
2022-08-25 14:54:47 +0200

    fix(java): more flexible json parser (#954)

--- clients/algoliasearch-client-java-2/algoliasearch-core/src/main/java/com/algolia/utils/JSONBuilder.java
+++ clients/algoliasearch-client-java-2/algoliasearch-core/src/main/java/com/algolia/utils/JSONBuilder.java
@@ -3,6 +3,7 @@ package com.algolia.utils;
 import com.fasterxml.jackson.annotation.*;
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.json.JsonMapper;
+import static com.fasterxml.jackson.core.JsonGenerator.Feature;
 
 public class JSONBuilder {
 
@@ -19,8 +20,11 @@ public class JSONBuilder {
     ObjectMapper mapper = JsonMapper.builder().disable(MapperFeature.ALLOW_COERCION_OF_SCALARS).build();
     mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
     mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, this.failOnUnknown);
+    mapper.enable(Feature.AUTO_CLOSE_JSON_CONTENT);
     mapper.enable(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE);
     mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    mapper.disable(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS);
+    mapper.disable(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS);
     mapper.enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING);
     mapper.enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING);
     return mapper;
